70d80802bf5960e3e476edb95c8714e3d04b902a
aluno <gustavo.sperandio25@gmail.com>
2024-11-25 21:15:19 -0300

    First Commit

---
+++ Tasks/Controllers/TasksController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Tasks.Models;
+
+namespace Tasks.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        private static List<ModelTask> modelTasks = 
+                new List<ModelTask>();
+
+
+        [HttpGet]
+        public ActionResult<List<ModelTask>> SearchTasks()
+        {
+            return Ok(modelTasks);
+        }
+
+        [HttpPost]
+        public ActionResult<List<ModelTask>> AddTask(ModelTask newTask)
+        {
+            if (newTask.Description.Length < 10)
+                return BadRequest("Need more characters");
+
+            newTask.Id = modelTasks.Count > 0
+                                ? modelTasks[modelTasks.Count - 1].Id + 1
+                                : 1;
+
+            modelTasks.Add(newTask);
+            return Ok(modelTasks);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult RemoveTask(int id)
+        {
+            var finded = modelTasks.Find(x => x.Id == id);
+
+            if (finded is null)
+                return NotFound("This task doesn't exist");
+
+            modelTasks.Remove(finded);
+            return Ok(); 
+        }
+
+    }
+}
